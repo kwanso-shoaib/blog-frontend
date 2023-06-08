82edb3486d1937c395b2efd7b27a5eef05fbb905
Shoaib Hassan <ayyaz@shoaib-hassan.localdomain>
2023-06-08 17:36:21 +0500

    pagination is working now

--- src/container/HomeContainer.tsx
+++ src/container/HomeContainer.tsx
@@ -1,36 +1,36 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { PostContext } from "../context/post";
 import { BlogCardsList } from "../components";
 import { BLOGS_PER_PAGE } from "../constants";
 import { Box } from "@mui/material";
-import { Posts } from "../gql/graphql";
+import { Posts, useFindAllPostsQuery } from "../gql/graphql";
+import { PostActions } from "../reducers/post";
 
 export const HomeConainer = () => {
+  const [page, setPage] = useState<number>(0);
   const { posts, count, dispatchPostAction } = useContext(PostContext);
-  // const {
-  //   data: allPosts,
-  //   loading,
-  //   refetch,
-  // } = useFindAllPostsQuery({
-  //   variables: {
-  //     skip: 0,
-  //     take: BLOGS_PER_PAGE,
-  //   },
-  // });
+  const { data: allPosts } = useFindAllPostsQuery({
+    variables: {
+      skip: page,
+      take: BLOGS_PER_PAGE,
+    },
+  });
 
-  const onRefetch = (page: number) => {
-    //refetch({ skip: BLOGS_PER_PAGE * (page - 1), take: BLOGS_PER_PAGE });
+  const onRefetch = (pages: number) => {
+    setPage(() => BLOGS_PER_PAGE * (pages - 1));
   };
 
-  // useEffect(() => {
-  //   dispatchPostAction({
-  //     type: PostActions.GET_POST,
-  //     payload: {
-  //       posts: allPosts?.findAllPosts.items,
-  //       count: allPosts?.findAllPosts.total,
-  //     },
-  //   });
-  // }, [allPosts]);
+  useEffect(() => {
+    if (allPosts != undefined) {
+      dispatchPostAction({
+        type: PostActions.GET_POST,
+        payload: {
+          posts: allPosts.findAllPosts.items,
+          count: allPosts.findAllPosts.total,
+        },
+      });
+    }
+  }, [allPosts]);
   return (
     <Box sx={{ marginTop: "48px" }}>
       {posts != undefined && (
